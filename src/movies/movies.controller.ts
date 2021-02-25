727f85487ce49a794cb226bf8945dd295bab89a5
DESKTOP-48B4POT\PLANIT <whdntjrsla12@naver.com>
2021-02-25 11:11:08 +0900

    study #2.7 Express on NestJS

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,7 +1,8 @@
 import { MoviesService } from './movies.service';
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, Req, Res} from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update.-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -10,7 +11,7 @@ export class MoviesController {
 
 
     @Get()
-    getAll():Movie[] {
+    getAll(@Req() req, @Res() res):Movie[] { //다음과 같이 express에 접근할 수 있다 하지만 추천하지 않는다. Fastify처럼 Express와는 다른 방법을 쓰고 싶을 수 있으니까
         return this.moviesService.getAll();
     }
 
@@ -32,7 +33,7 @@ export class MoviesController {
     }
 
     @Patch("/:id")
-    patch(@Param("id") movieId: number, @Body() updateData){
+    patch(@Param("id") movieId: number, @Body() updateData : UpdateMovieDto){
         return this.moviesService.update(movieId, updateData);
     }
 
